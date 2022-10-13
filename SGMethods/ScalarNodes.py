9e1872b7897130198674aaff30205167cc50da73
Andrea Scaglioni <andrea.scaglioni@asc.tuwien.ac.at>
2022-10-13 18:41:31 +0200

    new optimal nested nodes for interpolation on R$

--- SGMethods/ScalarNodes.py
+++ SGMethods/ScalarNodes.py
@@ -1,14 +1,9 @@
 from scipy.stats import norm
-from math import log, sqrt, log2
+from scipy.special import erfinv
+from math import log, sqrt, log2, exp, pi
 import numpy as np
 
 
-def unboundedKnotsNested(n):
-    """n must be of form 2^(i+1) - 1"""
-    assert (log2(n + 1) - 1 >= 0 & (abs(log2(n + 1) - int(log2(n + 1)) < 1.e-10)))
-    knotsBounded = np.linspace(0, 1, n+2)
-    knotsBounded = knotsBounded[1:-1:]
-    return norm.ppf(knotsBounded)
 
 def unboundKnotsNonNest(n):
     assert (n > 0)
@@ -20,3 +15,21 @@ def unboundKnotsNonNest(n):
     return norm.ppf(knotsBounded)
 
 
+def unboundedNodesOptimal(n):
+    assert(n%2==1)
+    m = int((n-1)/2)
+    xx = np.linspace(-m,m,n)/(m+1)
+    return 2**(3/2) * erfinv(xx)
+
+def unboundedKnotsNested(n):
+    """n must be of form 2^(i+1) - 1"""
+    assert (log2(n + 1) - 1 >= 0 & (abs(log2(n + 1) - int(log2(n + 1)) < 1.e-10)))
+    return unboundedNodesOptimal(n)
+
+# wrong distribution of nodes, thoruw away soon
+def unboundedKnotsNestedDEPRECATED(n):
+    """n must be of form 2^(i+1) - 1"""
+    assert (log2(n + 1) - 1 >= 0 & (abs(log2(n + 1) - int(log2(n + 1)) < 1.e-10)))
+    knotsBounded = np.linspace(0, 1, n+2)
+    knotsBounded = knotsBounded[1:-1:]
+    return norm.ppf(knotsBounded)
