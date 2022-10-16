43f41d35c177528ed0f482fbf6a0c61e8829659a
Andrea Scaglioni <andrea.scaglioni@asc.tuwien.ac.at>
2022-10-16 11:23:48 +0200

    added max # dimensions for midSet datastructure

--- SGMethods/MidSets.py
+++ SGMethods/MidSets.py
@@ -1,5 +1,5 @@
 import numpy as np
-from math import floor, log
+from math import floor, log, inf
 
 # DEPRECATED using np.meshgrid for N>32 not allowed
 # def TPMidSet(w, N):
@@ -88,36 +88,36 @@ def LLG_optimal_midset_freeN(T, rhoFun):
     assert(rho.size == N)
     return LLG_optimal_midset_recursive(N, T, rho)
 
-def LLG_optimal_midset_recursive(N, T, rho):
-    """INPUT N int number parametric dimensions
-             T int threshold parameter midset
-             rhoFun function; rhoFun(N) outputs a length N increasing list of positive doubles of 
-       OUTPUT \Lambda_T np array shape (# SG nodes, N) for kanpsack-type optimal multi-index set i.e.
-       Lambda_T = \left\{ \nu\in N^N : 2\vert\nu\vert_1 + \sum_{\nu_i>0} \log_2(\rho_i) + N \log_2(deg-1) \leq T}
-       NBB term  + N \log_2(deg-1) omitted because independent of \nu """
-    if T <= 0 :
-        return np.zeros((1, N), dtype=int)
-    elif N == 1:
-        b = max(0, floor(0.5*(T-log(rho[0], 2))) )
-        I = np.linspace(0, b, b+1)
-        I = np.reshape(I, (1,)+I.shape)
-        return I.T.astype(int)
-    else: # T>0 and N>1. recursion by elimination of first coordinate
-        I = np.zeros((0, N), dtype=int)
-        LIM = max(0, floor(0.5*(T-log(rho[0], 2))))
-        # case nu_1 = 0
-        nu1=0
-        rest = LLG_optimal_midset_recursive(N-1, T, rho[1::])
-        newFirstCol = nu1*np.ones((rest.shape[0], 1), dtype=int)
-        newRows = np.concatenate((newFirstCol, rest), 1)
-        I = np.concatenate((I, newRows), 0)
-        # case nu_1>0
-        for nu1 in range(1, LIM+1):  # nbb last int in range() is NOT included!!
-            rest = LLG_optimal_midset_recursive(N-1, T - 2*nu1 - log(rho[0], 2), rho[1::])
-            newFirstCol = nu1*np.ones((rest.shape[0], 1), dtype=int)
-            newRows = np.concatenate((newFirstCol, rest), 1)
-            I = np.concatenate((I, newRows), 0)
-        return I.astype(int)
+# def LLG_optimal_midset_recursive(N, T, rho):
+#     """INPUT N int number parametric dimensions
+#              T int threshold parameter midset
+#              rhoFun function; rhoFun(N) outputs a length N increasing list of positive doubles of 
+#        OUTPUT \Lambda_T np array shape (# SG nodes, N) for kanpsack-type optimal multi-index set i.e.
+#        Lambda_T = \left\{ \nu\in N^N : 2\vert\nu\vert_1 + \sum_{\nu_i>0} \log_2(\rho_i) + N \log_2(deg-1) \leq T}
+#        NBB term  + N \log_2(deg-1) omitted because independent of \nu """
+#     if T <= 0 :
+#         return np.zeros((1, N), dtype=int)
+#     elif N == 1:
+#         b = max(0, floor(0.5*(T-log(rho[0], 2))) )
+#         I = np.linspace(0, b, b+1)
+#         I = np.reshape(I, (1,)+I.shape)
+#         return I.T.astype(int)
+#     else: # T>0 and N>1. recursion by elimination of first coordinate
+#         I = np.zeros((0, N), dtype=int)
+#         LIM = max(0, floor(0.5*(T-log(rho[0], 2))))
+#         # case nu_1 = 0
+#         nu1=0
+#         rest = LLG_optimal_midset_recursive(N-1, T, rho[1::])
+#         newFirstCol = nu1*np.ones((rest.shape[0], 1), dtype=int)
+#         newRows = np.concatenate((newFirstCol, rest), 1)
+#         I = np.concatenate((I, newRows), 0)
+#         # case nu_1>0
+#         for nu1 in range(1, LIM+1):  # nbb last int in range() is NOT included!!
+#             rest = LLG_optimal_midset_recursive(N-1, T - 2*nu1 - log(rho[0], 2), rho[1::])
+#             newFirstCol = nu1*np.ones((rest.shape[0], 1), dtype=int)
+#             newRows = np.concatenate((newFirstCol, rest), 1)
+#             I = np.concatenate((I, newRows), 0)
+#         return I.astype(int)
 
 
 def count_cps(I, r):
@@ -133,29 +133,24 @@ def count_cps(I, r):
 Start from {0}
 New dimensions are also added as follows: I keep an empty dimenision as buffer, when a fully fimensional mid is added, increase the buffer by 1"""
 class midSet():
-    def __init__(self):
+    def __init__(self, maxN=inf):
         self.N = 0  ## NBB self.midset.shape[1] is always N+1 (not N) because of buffer
+        self.maxN = maxN  # maximum number of allowed dimensions
         self.midSet = np.zeros((1, 1)).astype(int)
         self.numMids = self.midSet.shape[0]
         self.margin = np.identity(1).astype(int)
-        
     def update(self, idx_margin):
-        
         mid = self.margin[idx_margin,:]
-        # sanity check for dimensinality
-        assert(mid[-1] >= 0 and mid[-1] <= 1)
-        # check if in reduced margin, 
+        # assert(mid[-1] >= 0 and mid[-1] <= 1) # sanity check for dimensinality (buffer dim margin should always be empty)
+        # check if mid is in reduced margin
         for n in range(self.N+1):
             en = np.zeros(self.N+1).astype(int)
             en[n] = 1
             assert(mid[n] == 0 or ((mid-en).tolist() in self.midSet.tolist()))
-
         #update midset
         self.midSet = np.row_stack((self.midSet, self.margin[idx_margin, :]))
-        # self.midSet = np.sort(self.midSet, axis=1)
         self.midSet =self.midSet[np.lexsort(np.rot90(self.midSet))]
         self.numMids += 1
-        
         #update margin 
         self.margin = np.delete(self.margin, idx_margin,  0)  # remove added mid 
         for n in range(self.N+1): # add those in forward margin (if they are in not already in margin!)
@@ -165,39 +160,12 @@ class midSet():
             newMid = mid + en
             if(not(newMid.tolist() in self.margin.tolist())):
                 self.margin = np.row_stack((self.margin, newMid))
-        
         # update number of dimensions N of midset and margin if added mid had nonzero last dimension. Also add (I,1) to margin
-        if(mid[-1] == 1):
+        if(mid[-1] == 1 and self.N < self.maxN):
             self.N = self.N + 1
-            
             marginNewDim = np.hstack((self.midSet, np.ones((self.midSet.shape[0], 1))))
-            
             self.midSet = np.hstack((self.midSet, np.zeros((self.midSet.shape[0], 1)))).astype(int)
             self.margin = np.hstack((self.margin, np.zeros((self.margin.shape[0], 1))))
-                        
             self.margin = np.vstack((self.margin, marginNewDim)).astype(int)
-       
         # finally sort the margin in lexicographic order
-        self.margin =self.margin[np.lexsort(np.rot90(self.margin))]
-        # self.margin = np.sort(self.margin, axis=1)
-        
-        
-
-# def optimalProfitMidSet(n, N, P, Iold=None):
-#     """ Create a class midSet instance containing the n mids in N^N with maximum profit 
-#         P(\nu)
-#         Start with I_1 = {0} and grow it 1 mid at a time on margin"""
-#     I = midSet(N)
-#     alpha=2
-#     beta=0.5
-#     rho = 2**(beta*np.ceil(np.log(np.linspace(1,N,N))))
-#     ProfitFun = lambda x : 2**((2*alpha-1)*np.sum(x))# - alpha * np.sum(np.log2(rho,2), where=(x>0)))
-#     while I.numMids() < n:
-#         # find profit maximizer NBB must be in reduced margin!
-#         P = np.apply_along_axis(ProfitFun, 1, I.margin)
-#         print(P)
-#         idMax = np.argmax(P)
-#         # add it in right place
-#         I.update(idMax)
-#     return I
-    
+        self.margin = self.margin[np.lexsort(np.rot90(self.margin))]
