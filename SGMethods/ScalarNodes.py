7390683d7df994f377e1296a21a5ef1af111014e
Andrea Scaglioni <andrea.scaglioni@asc.tuwien.ac.at>
2022-10-22 11:29:02 +0200

    generalized optimal nodes for any p

--- SGMethods/ScalarNodes.py
+++ SGMethods/ScalarNodes.py
@@ -4,7 +4,6 @@ from math import log, sqrt, log2, exp, pi
 import numpy as np
 
 
-
 def unboundKnotsNonNest(n):
     assert (n > 0)
     assert (n % 2 == 1)
@@ -14,22 +13,38 @@ def unboundKnotsNonNest(n):
     knotsBounded = knotsExtrema[0] + (knotsExtrema[1] - knotsExtrema[0]) * np.linspace(0, 1, int(n))
     return norm.ppf(knotsBounded)
 
-
-def unboundedNodesOptimal(n):
-    assert(n%2==1)
-    m = int((n-1)/2)
-    xx = np.linspace(-m,m,n)/(m+1)
-    return 2**(3/2) * erfinv(xx)
-
-def unboundedKnotsNested(n):
-    """n must be of form 2^(i+1) - 1"""
-    assert (log2(n + 1) - 1 >= 0 & (abs(log2(n + 1) - int(log2(n + 1)) < 1.e-10)))
-    return unboundedNodesOptimal(n)
-
-# wrong distribution of nodes, thoruw away soon
+# wrong distribution of nodes, throw away soon
 def unboundedKnotsNestedDEPRECATED(n):
     """n must be of form 2^(i+1) - 1"""
     assert (log2(n + 1) - 1 >= 0 & (abs(log2(n + 1) - int(log2(n + 1)) < 1.e-10)))
     knotsBounded = np.linspace(0, 1, n+2)
     knotsBounded = knotsBounded[1:-1:]
     return norm.ppf(knotsBounded)
+
+
+def unboundedNodesOptimal(n, p=1):
+    assert(n%2==1)
+    m = int((n-1)/2)
+    xx = np.linspace(-m,m,n)/(m+1)
+    return 2*sqrt(2*p) * erfinv(xx)
+
+def unboundedKnotsNested(n, p=1):
+    """n must be of form 2^(i+1) - 1"""
+    assert (log2(n + 1) - 1 >= 0 & (abs(log2(n + 1) - int(log2(n + 1)) < 1.e-10)))
+    return unboundedNodesOptimal(n, p)
+
+
+
+def unboundedKnotsTest(n):
+    assert(n%2==1)
+    m = int((n-1)/2)
+    xx = np.linspace(-m,m,n)/(m+1)
+    return 2*sqrt(5) * erfinv(xx)
+
+def unboundedKnotsTest2(n):
+    assert(n%2==1)
+    m = int((n-1)/2)
+    xx = np.linspace(1, m, m)/(m+1)
+    v = 2*sqrt(2) * erfinv(xx)
+    vr = np.sort(-v)
+    return np.hstack((vr, np.array([0.]), v))
--- test5.py
+++
@@ -1,3 +0,0 @@
-
-for i in range(5,4):
-    print(i)
