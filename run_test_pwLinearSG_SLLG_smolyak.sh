cbcc712f321b50e9742b22a30a9b17bd2b81feb0
andrea <andrea.c.scaglioni@gmail.com>
2022-10-23 11:27:21 +0200

    shell scripts

---
+++ run_test_pwLinearSG_SLLG_smolyak.sh
@@ -0,0 +1,2 @@
+nohup python3 < SLLG/test_pwLinearSG_SLLG_Smolyak.py > out_pwLinearSG_SLLG_Smolyak.txt &
+
---
+++ run_test_pwQuadraticSG_SLLG_fixN.sh
@@ -0,0 +1,2 @@
+nohup python3 < SLLG/test_pwQuadraticSG_SLLG_fixN.py > out_pwQuadraticSG_SLLG_fixN.txt &
+
