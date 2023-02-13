re-intrduced correct scaling unbounded knots

SGMethods/ScalarNodes.py
@@ -13,17 +13,21 @@ def unboundKnotsNonNest(n):
     knotsBounded = knotsExtrema[0] + (knotsExtrema[1] - knotsExtrema[0]) * np.linspace(0, 1, int(n))
     return norm.ppf(knotsBounded)
 
-def unboundedNodesOptimal(n, p=1):
+def unboundedNodesOptimal(n, p=2):
     """ n number of nodes
-        p degree of interpolant
+        p int number of interpolation pts, so degree+1 !!
         return np.array length n of optimal nodes for order p interpolant"""    
     assert(n%2==1)
     m = int((n-1)/2)
     xx = np.linspace(-m,m,n)/(m+1)
-    return 2*sqrt(2*(p+1)) * erfinv(xx)
+    c = 2 *sqrt(2*p)
+    return c * erfinv(xx)
+    
 
-def unboundedKnotsNested(n, p=1):
-    """n must be of form 2^(i+1) - 1"""
+def unboundedKnotsNested(n, p=2):
+    """ n number of nodes must be of form 2^(i+1) - 1
+        p int number of interpolation pts, so degree+1 !!
+        return np.array length n of optimal nodes for order p interpolant """
     assert (log2(n + 1) - 1 >= 0 & (abs(log2(n + 1) - int(log2(n + 1)) < 1.e-10)))
     return unboundedNodesOptimal(n, p)
 