01b621a1e3f9c141df3ce6c6294a69c041371243
Andrea Scaglioni <andrea.scaglioni@asc.tuwien.ac.at>
2023-07-12 08:45:44 +0200

    some SGMethods tests

--- tests/test_sinW.py
+++ tests/test_sinW.py
@@ -9,10 +9,10 @@ from SGMethods.MidSets import midSet
 from SGMethods.SGInterpolant import SGInterpolant
 from SGMethods.ScalarNodes import unboundedKnotsNested
 from SLLG.expansions_Brownian_motion import param_LC_Brownian_motion
-
+from SLLG.profits import ProfitMix
 
 NRNDSamples = 100
-maxNumNodes = 100
+maxNumNodes = 1024
 p=2
 interpolationType =  "linear"
 
@@ -20,18 +20,15 @@ interpolationType =  "linear"
 Nt = 100
 tt = np.linspace(0, 1, Nt)
 dt = 1/Nt
-
 def F(x):
     return np.sin(param_LC_Brownian_motion(tt, x, 1))
-
 def computeL2Error(uExa, Iu):
     assert(uExa.shape == Iu.shape)
     spaceNorm = lambda x : sqrt(dt)*np.linalg.norm(x, ord=2, axis=1)  # L2 norm in time 
     errSample = spaceNorm(uExa-Iu)
     return sqrt(np.mean(np.square(errSample)))
-
 np.random.seed(1607)
-yyRnd = np.random.normal(0, 1, [NRNDSamples, 2**10])  # infinite paramter vector
+yyRnd = np.random.normal(0, 1, [NRNDSamples, 1000])  # infinite paramter vector
 print("Parallel random sampling")
 NParallel = 1
 if NParallel == 1:
@@ -46,36 +43,9 @@ else:
 # Choose interpolant
 lev2knots = lambda n: 2**(n+1)-1
 knots = lambda n : unboundedKnotsNested(n, p=p)
-def Profit(nu):
-    if(len(nu.shape) == 1):
-        nu= np.reshape(nu, (1,-1))
-    nMids = nu.shape[0]
-    nDims = nu.shape[1]
-    rho = 2**(0.5*np.ceil(np.log2(np.linspace(1,nDims,nDims))))
-    repRho = np.repeat(np.reshape(rho, (1, -1)), nMids, axis=0)
-    # c = sqrt(pi**p * 0.5**(-(p+1)) * (2*p+1)**(0.5*(2*p-1)) ) * np.sqrt(1 + (2**(2*p+2)-4)/(2**(nu+1)))
-    # C1 = 1+ c * 3**(-p)/factorial(p)
-    # C2 = c * (1+2**(-p))/factorial(p)
-    C1=1
-    C2=1
-    v1 = np.prod(C1/repRho, axis=1, where=(nu==1))
-    v2 = np.prod(C2*np.power(2**nu * repRho, -p) ,axis=1, where=(nu>1))
-    w = np.prod((2**(nu+1)-2)*(p-1)+1 ,axis=1)
-    return v1 * v2 / w
+Profit = lambda nu : ProfitMix(nu, p)
 
-def Profit2(nu):
-    if(len(nu.shape) == 1):
-        nu= np.reshape(nu, (1,-1))
-    nMids = nu.shape[0]
-    nDims = nu.shape[1]
-    rho = 2**(0.5*np.ceil(np.log2(np.linspace(1,nDims,nDims))))
-    repRho = np.repeat(np.reshape(rho, (1, -1)), nMids, axis=0)
-
-    v = np.prod(np.power(2**nu * repRho, -p) ,axis=1, where=(nu>0))
-    w = np.prod((2**(nu+1)-2)*(p-1)+1 ,axis=1)
-    return v/w
-
-def convergenceTest(ProfitFun):
+def convergenceTest(Profit):
     # convergence test
     err = np.array([])
     nNodes = np.array([])
@@ -98,28 +68,26 @@ def convergenceTest(ProfitFun):
         print("Error:", err[-1])
         nNodes = np.append(nNodes, interpolant.numNodes)
         nDims = np.append(nDims, I.N)
+
+
+        plt.plot(tt, uInterp[0], '.-')
+        plt.plot(tt, uExa[0], 'k-')
+        plt.show()
+
         
-        np.savetxt('sinW_example_linear_empricalP2.csv',np.transpose(np.array([nNodes, err, nDims])), delimiter=',')
+        np.savetxt('example_sinW.csv',np.transpose(np.array([nNodes, err, nDims])), delimiter=',')
 
         oldSG = interpolant.SG
 
-        # update midset for next iteration
-        # P = ProfitFun(I.margin)
-        # idMax = np.argmax(P)
         ncps = interpolant.numNodes
         while interpolant.numNodes < sqrt(2)*ncps:
-            P = ProfitFun(I.margin)
+            P = Profit(I.margin)
             idMax = np.argmax(P)
             I.update(idMax)
             interpolant = SGInterpolant(I.midSet, knots, lev2knots, interpolationType=interpolationType, NParallel=NParallel)
             if(interpolant.numNodes > maxNumNodes):
                 break
         w+=1
-    
-    # tt = np.linspace(0, 1, Nt)
-    # plt.plot(tt, uExa[0,:], '.-', tt, uInterp[0,:], '.-')
-    # plt.show()
-
     print(err)
     rates = -np.log(err[1::]/err[:-1:])/np.log(nNodes[1::]/nNodes[:-1:])
     print("Rate:",  rates)
@@ -127,6 +95,5 @@ def convergenceTest(ProfitFun):
     plt.loglog(nNodes, np.power(nNodes, -0.5), '-k')
     plt.loglog(nNodes, np.power(nNodes, -0.25), '-k')
 
-
 convergenceTest(Profit)
-plt.show()
+# plt.show()
