cf6ce2b0652a3647e08276c4c78750352a9fe10b
Andrea Scaglioni <andrea.scaglioni@asc.tuwien.ac.at>
2024-07-05 17:19:12 +0200

    modified tuttorial dir

---
+++ tutorials/2-interpolation_parametric_lognormal_Poisson.py
@@ -0,0 +1,57 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+from math import pi, sqrt
+import sys, os
+sys.path.insert(1, os.path.join(os.path.expanduser("~"), 'workspace/SGMethods'))
+from SGMethods.ScalarNodes import CCNodes
+from SGMethods.TPLagrangeInterpolator import TPLagrangeInterpolator
+from SGMethods.MidSets import anisoSmolyakMidSet
+from SGMethods.SGInterpolant import SGInterpolant
+from tutorials.sample_affine_diffusion_Poisson import sampleParametricPoisson, computeErrorSamplesPoisson
+
+"""Tutorial on sparse grid interpolation with SGMethods. 
+We see how to use SGMethods to approximate the parametric lognormal diffusion Poisson problem."""
+
+
+# Problem parameters
+np.random.seed(36157)
+N = 10  # number of parametric dimensions
+C = pi**2/6 * 1.1  # normalization diffusion coefficient
+a_min = 1 - -pi**2/(6*C)  # minimum diffusion coefficient
+nH = 16 # mesh resolution i.e. # edges per side of square domain mesh
+def F(y):  # target function sampler
+    return sampleParametricPoisson(y, nH=nH, orderDecayCoefficients=2)  # Sample the parametric Poisson problem
+
+# Error computation
+yyRnd = np.random.uniform(-1, 1, (128, 1000))  # Random parameters for MC error estimation
+uExa = np.squeeze(np.array(list(map(F, yyRnd))))  # Random sample of exact function
+
+# Sparse grid parameters
+TPInterpolant = lambda nodesTuple, fOnNodes : TPLagrangeInterpolator(nodesTuple, fOnNodes)
+lev2knots = lambda n: np.where(n==0, 1, 2**n+1)  # doubling rule:  1 if n==0 else 2**(n)-1
+knots = lambda n : CCNodes(n)  # Clenshaw-Curtis nodes
+
+# Anisotrpopy vector for parametric Poisson
+gamma = lambda N : 1/(C*a_min*np.linspace(1, N, N)**2)
+tau = lambda N : 1/(2*gamma(N))
+anisoVec = lambda N : 0.5* np.log(1+tau(N))  
+midSet = anisoSmolyakMidSet(w=2, N=N, a=anisoVec(N))  # Anisotropic Smolyak multi-index set
+interpolant = SGInterpolant(midSet, knots, lev2knots, TPInterpolant, NParallel=8)  # Spars grid interpolant
+uOnSG = interpolant.sampleOnSG(F)  # Sample the function on the sparse grid
+uInterp = interpolant.interpolate(yyRnd, uOnSG)  # Compute the interpolated value
+errSamples = computeErrorSamplesPoisson(uInterp, uExa, nH)  # Compute error samples in H^1_0(D)
+errorL2 = np.mean(errSamples)  # L^2 error in parameter space
+print("Error:", errorL2)
+print("Sparse grid:\n", interpolant.SG)
+
+# refine SG and compute refinement
+midSet2 = anisoSmolyakMidSet(w=4, N=N, a=anisoVec(N))
+interpolant2 = SGInterpolant(midSet2, knots, lev2knots, TPInterpolant=TPInterpolant, NParallel=8)
+print("Number of collocation nodes:", interpolant.numNodes)
+# Sample on refined sparse grid and recycle old values
+uOnSG = interpolant.sampleOnSG(F, oldXx=interpolant.SG, oldSamples=uOnSG)
+uInterp = interpolant.interpolate(yyRnd, uOnSG,)
+errSamples = computeErrorSamplesPoisson(uInterp, uExa, nH)
+errorL2 = sqrt(np.mean(np.square(errSamples)))
+print("Error:", errorL2)
+print("Sparse grid:\n", interpolant2.SG)
--- tutorials/sample_affine_diffusion_Poisson.py
+++ tutorials/sample_affine_diffusion_Poisson.py
@@ -4,37 +4,72 @@ from fenics import set_log_level, DOLFIN_EPS, Constant, Expression,UnitSquareMes
     TrialFunction, TestFunction, inner, grad, dx, solve, Function, norm
 
 
-def sampleParametricPoisson(yy, nH, orderDecayCoefficients):
-    """Sample the parameter-to-finite element solution map of the affine diffusion parametric Poisson Problem 
-        -\nabla \cdot (a(\by) \nabla u(\by)) = 1 on D
-                                           u = 0 on \partial D
-    Domain D is 2D unit square;
-    Diffusion a(\by) is affine in y, coercive on D
-    Parameters are in [-1,1], any length of yy is allowed
-    Mesh on D is structured with nH edges on each side
+def parametric_affine_diffusion(yy, orderDecayCoefficients):
+    """"Affine parametric diffusion for Poisson problem
+        a(\by, \bx) = 1 + \sum_{n=1}^{N} y_n*\sin(x_1*2.*pi*n)/(C*n^r)
     Args:
         yy (double array): Vector of scalar parameters in [-1,1]
-        nH (int): Number of mesh edges on each edge of square domain (structured mesh)
         orderDecayCoefficients (positive int): Order of decay terms affine diffusion
     
     Returns:
-        np.array: Corrdinates of the solution (w.r.t. selected hat functions basis)
-    """
-    
+        Expression: Affine parametric diffusion as a Dolfin Expression, for the fixed given parameter yy"""
+
+
     assert(np.min(yy) >= -1 and np.max(yy) <= 1)
-    set_log_level(31)  # reduce fenics logging
-    def boundary(x):
-        return x[0] < DOLFIN_EPS or x[0] > 1.0 - DOLFIN_EPS or x[1] < DOLFIN_EPS or x[1] > 1.0 - DOLFIN_EPS 
-    u0 = Constant(0.0)  # boundary condition
-    f = Constant(1.)  # right-hand-side
-    # Diffusion coefficient 
     C = pi**2/6 * 1.1  # makes the function uniformly positive
     strA = "1."
     for n in range(len(yy)):
         strA = strA + "+" + "sin(x[0]*2.*pi*" + str(n+1) + ")/("+str(C)+"*pow(" + str(n+1) + ","+ \
             str(orderDecayCoefficients) + "))" + "*" + str(yy[n])
-    a = Expression(strA, degree=2)
-    # Define computational objects
+    return Expression(strA, degree=2)
+
+def parametric_lognormal_diffusion(yy, orderDecayCoefficients):
+    """Lognormal parametric diffusion for Poisson problem
+        a(\by, \bx) = a_*(\bx) + a_0(\bx)*exp(\sum_{n=1}^{\infty} y_n \phi_n(x))
+    Args:
+        yy (double array): Vector of scalar parameters, unbounded!
+        orderDecayCoefficients (positive int): Order of decay terms affine diffusion
+    
+    Returns:
+        Expression: Affine parametric diffusion as a Dolfin Expression, for the fixed given parameter yy"""
+
+
+    as = 0
+    a_0 = 1
+
+
+    C = pi**2/6 * 1.1  # makes the function uniformly positive
+    strA = "1."
+    for n in range(len(yy)):
+        strA = strA + "+" + "sin(x[0]*2.*pi*" + str(n+1) + ")/("+str(C)+"*pow(" + str(n+1) + ","+ \
+            str(orderDecayCoefficients) + "))" + "*" + str(yy[n])
+    return Expression(strA, degree=2)
+
+def sampleParametricPoisson(yy, nH, orderDecayCoefficients):
+    """Sample the parameter-to-finite element solution map of the affine diffusion parametric Poisson Problem 
+        -\nabla \cdot (a(\by) \nabla u(\by)) = 1 on D
+                                           u = 0 on \partial D
+    Domain D is 2D unit square;
+    Diffusion a(\by) is coercive on D for any \by (n.b. coercivity constant needs not be unform in \by!)
+    Parameters are in [-1,1], any length of yy is allowed
+    Mesh on D is structured with nH edges on each side
+    Args:
+        yy (double array): Vector of scalar parameters
+        nH (int): Number of mesh edges on each edge of square domain (structured mesh)
+        orderDecayCoefficients (positive int): Order of decay terms affine diffusion
+    
+    Returns:
+        np.array: Corrdinates of the solution (w.r.t. selected hat functions basis)"""
+    
+    
+    set_log_level(31)  # reduce fenics logging
+    # Problem data
+    def boundary(x):
+        return x[0] < DOLFIN_EPS or x[0] > 1.0 - DOLFIN_EPS or x[1] < DOLFIN_EPS or x[1] > 1.0 - DOLFIN_EPS 
+    u0 = Constant(0.0)  # boundary condition
+    f = Constant(1.)  # right-hand-side
+    a = parametric_affine_diffusion(yy, orderDecayCoefficients)  # Affine para,etric diffusion as a Dolfin Expression
+    # Computational objects
     mesh = UnitSquareMesh(nH, nH)
     V = FunctionSpace(mesh, "Lagrange", 1)
     bc = DirichletBC(V, u0, boundary)
@@ -43,8 +78,9 @@ def sampleParametricPoisson(yy, nH, orderDecayCoefficients):
     L = inner(a*grad(u), grad(v))*dx
     rhs = f*v*dx
     u = Function(V)
-    solve(L == rhs, u, bc)  # solve linear system
-    return u.vector()[:]
+    # Solve linear system
+    solve(L == rhs, u, bc)
+    return u.vector()[:]  # return only coordinates!
 
 def computeErrorSamplesPoisson(u, uExa, nH):
     """Compute H^1_0(D) norm (see brevious function) of difference of two functions giventhrough their coordinates in\
