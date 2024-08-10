84bcaa04ff58498bf24b7b883971a02b47009656
andreascaglioni <andrea.c.scaglioni@gmail.com>
2024-08-10 16:59:42 +0200

    renamed source dir to SRC

--- tests/test_nodes_1d.py
+++ tests/test_nodes_1d.py
@@ -5,28 +5,27 @@ from scipy.special import erfinv
 from SGMethods.nodes_1d import unboundedKnotsNested
 
 
-
 class Test1Node(unittest.TestCase):
     def test_list_int(self):
-        """
-        Test that the 1 point nodes family is just (0.)
+        """ Test that the 1 point nodes family is just (0.)
         """
         y = unboundedKnotsNested(1)
         self.assertAlmostEqual(y, np.array([0.]))
+
 
 if __name__ == '__main__':
     unittest.main()
 
 
-
 def unboundedKnotsTest(n):
-    assert(n%2==1)
+    assert (n % 2 == 1)
     m = int((n-1)/2)
-    xx = np.linspace(-m,m,n)/(m+1)
+    xx = np.linspace(-m, m, n)/(m+1)
     return 2*sqrt(5) * erfinv(xx)
 
+
 def unboundedKnotsTest2(n):
-    assert(n%2==1)
+    assert (n % 2 == 1)
     m = int((n-1)/2)
     xx = np.linspace(1, m, m)/(m+1)
     v = 2*sqrt(2) * erfinv(xx)
