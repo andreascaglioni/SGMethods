c35aa1f3e5e2f444bfef2da2d3fbbec4b323f6d5
Andrea Scaglioni <andrea.scaglioni@asc.tuwien.ac.at>
2024-10-04 16:56:32 +0200

    reorg tutorials

---
+++ tutorials/1_interpolate_scalar_function_unbounded_domain.py
@@ -0,0 +1,41 @@
+import numpy as np 
+from math import sin, sqrt
+from scipy.interpolate import RegularGridInterpolator
+import sys, os
+sys.path.insert(1, os.path.join(os.path.expanduser("~"), 'workspace/SGMethods'))
+from src.nodes_1d import CCNodes
+from src.multi_index_sets import anisoSmolyakMidSet
+from src.tp_inteprolants import TPPwLinearInterpolator
+from src.sparse_grid_interpolant import SGInterpolant
+
+
+""" SGMethods Tutorial 0: Interpolate a scalar function.
+We use SGMethods to interpolate a function F : [-1,1]^N rightarrow R.
+The domain has dimension N in N, possibly high.
+"""
+
+# High-dimensional function
+weightFun = lambda N : np.power(np.linspace(1, N, N), -2)  # Weight function
+def F(y):
+    weightCurr = weightFun(y.size)
+    return sin(np.dot(weightCurr, y))
+
+# Sparse grid parameters. 
+p = 2  # Polynomial degree of the interpolant is p-1 ine ach parameter
+lev2knots = lambda nu: np.where(nu==0, 1, 2**(nu-1)+1)  # Level-to-knots function
+knots = lambda n : CCNodes(n)  # Nodes family
+
+# Error computation
+def computeL2Error(uExa, Iu):
+    assert(uExa.shape[0] == Iu.shape[0])
+    return sqrt(np.mean(np.square(uExa - Iu)))
+yyRnd = np.random.normal(0, 1, [256, 1000])  # 256 random sample of (effectively) infinite parameter vector
+uExa = np.array(list(map(F, yyRnd)))  # Random sample of exact function
+
+# Sparse grid interpolation
+anisoVec = lambda N : 2**(np.linspace(0, N-1, N))
+midSet = anisoSmolyakMidSet(w=5, N=10, a=anisoVec(10))  # Anisotropic Smolyak multi-index set in 10 dimensions
+interpolant = SGInterpolant(midSet, knots, lev2knots, TPInterpolant=TPInterpolant)
+uOnSG = interpolant.sampleOnSG(F)  # Sample the function on the sparse grid
+uInterp = interpolant.interpolate(yyRnd, uOnSG)  # Compute the interpolated value
+error = computeL2Error(uExa, uInterp)
