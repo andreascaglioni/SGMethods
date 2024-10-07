6d359eb92af930897c7c6a3fd60d82460ffea5ba
Andrea Scaglioni <andrea.scaglioni@asc.tuwien.ac.at>
2024-10-07 20:13:23 +0200

    added docs

--- sgmethods/multi_index_sets.py
+++ sgmethods/multi_index_sets.py
@@ -1,29 +1,33 @@
+"""A collection of functions to generate dowward-closed multi-index sets."""
+
+from math import floor
 import numpy as np
-from math import floor, inf
 from sgmethods.utils import coordUnitVector as unitVector
-from sgmethods.utils import lexicSort, midIsInReducedMargin, find_idx_in_margin, \
-    findMid, lexicSort
+from sgmethods.utils import lexicSort
 
 
-"""A collection of functions to generate multi-index sets"""
+def tensor_product_mid_set(w, N):
+    r"""Generate a tensor product multi-index set, i.e.
 
+    .. math::
 
-def TPMidSet(w, N):
-    """Generate a tensor product multi-index set such that
-        max_i=0^{N-1} nu_i leq w
-        NB leq so for w=0 only multi-index 0 included
+        \Lambda = \{\nu\in\mathbb{N}_0^N : \sum_{n=1}^N \nu_n \leq w\},
+        
+    where we denoted :math:`\Lambda` the multi-index set, and :math:`w, N` the 
+    input ``w``, ``N``.
 
     Args:
-        w (int): maximum l^infty norm of multi-index 
-        N (int): number of dimensions
+        w (float): Sizing parameter fo the multi-index set.
+        N (int): Number of dimensions.
 
     Returns:
-        np.array: multi-index set (each row is a multi-index)
+        numpy.ndarray[int]: Multi-index set (each row is a multi-index of length
+        ``N``).
     """
 
-    assert (N >= 1)
-    assert (w >= 0)
-    
+    assert N >= 1
+    assert w >= 0
+
     if w == 0:
         return np.zeros([1, N], dtype=int)
     elif N == 1:
@@ -33,54 +37,75 @@ def TPMidSet(w, N):
     else:  # w>0 & N >1
         I = np.zeros((0, N), dtype=int)
         for comp1 in range(w+1):
-            rest = TPMidSet(w, N-1)
-            newFirstCol = comp1*np.ones((rest.shape[0], 1), dtype=int)
-            newRows = np.concatenate((newFirstCol, rest), 1)
-            I = np.concatenate((I, newRows), 0)
+            rest = tensor_product_mid_set(w, N-1)
+            new_first_col = comp1*np.ones((rest.shape[0], 1), dtype=int)
+            new_rows = np.concatenate((new_first_col, rest), 1)
+            I = np.concatenate((I, new_rows), 0)
         return I
 
 
-def anisoSmolyakMidSet_freeN(w, a):
-    """Generate the anisotropic Smolyak multi-index set
-        set{ bnu in N_0^N : sum_i=1^N a_i*nu_i leq w}
-        . The dimension (N) is 
-        determined as: max {nin N : a_N leq w} 
+def aniso_smolyak_mid_set_free_N(w, a):
+    r"""Generate the anisotropic Smolyak multi-index set:
+
+    .. math::
+
+        \Lambda = \{\nu\in\mathbb{N}_0^N : \sum_{i=1}^N a_i\nu_i \leq w\},
+
+    where we denoted :math:`\Lambda` the multi-index set, and :math:`w, N` the 
+    input ``w``, ``N``. 
+    The dimensionality is determined as: 
+
+    .. math::
+
+        N = \min \{ N\in\mathbb{N} : 
+        \forall \nu\in\Lambda,\ \sum_{n=1}^N a_n\nu_n \leq w \}.
 
     Args:
-        w (double): maximum l^infty norm of multi-index 
-        a (function): a(N) gives anisotropy vector of length N
+        w (float): Sizing parameter fo the multi-index set.
+        a (Callable[[int], numpy.ndarray[float]]): a(N) gives the anisotropy 
+            vector of length :math:`N`. The anisotropy vector is a vector of
+            positive floats.
 
     Returns:
-        np.array: multi-index set (each row is a multi-index)
+        numpy.ndarray[int]: Multi-index set (each row is a multi-index).    
     """
 
     # Find the maximum N for which a cdot e_N leq w
     N = 1
-    while (a(N+1)[-1] <= w):
+    while a(N+1)[-1] <= w:
         N += 1
-    return anisoSmolyakMidSet(w, N, a(N))
+    return aniso_smolyak_mid_set(w, N, a(N))
 
 
-def anisoSmolyakMidSet(w, N, a):
-    """Generate the anisotropic Smolyak multi-index set:
-        set{ bnu in N_0^N : sum_i=1^N a_i*nu_i leq w}
+def aniso_smolyak_mid_set(w, N, a):
+    r"""Generate the anisotropic Smolyak multi-index set:
 
+    .. math::
+    
+        \Lambda = \{\nu \in \mathbb{N}_0^N : \sum_{i=1}^N a_i\nu_i \leq w \}.
+
+    where we denoted :math:`\Lambda` the multi-index set, and :math:`w, N` the 
+    input ``w``, ``N``.
+    
     Args:
-        w (double): Maximum l^infty norm of multi-index 
-        N (int): Number of dimensions
-        a (array double): Anisotropy vector (of length N)
+        w (float): Sizing parameter fo the multi-index set.
+        N (int): Number of dimensions.
+        a (Callable[[int], numpy.array[float]]): The resuting :math:`a(N)` gives
+            the anisotropy vector of length :math:`N`. The anisotropy vector is
+            a vector of positive floats.
 
     Returns:
-        np.array: Multi-index set (each row is a multi-index)
+        numpy.ndarray[int]: Multi-index set (each row is a multi-index of length
+        ``N``).
     """
 
     assert N >= 1
     assert w>=0
-    if type(a) == list:
+    if a.isinstance(list):
         a = np.array(a)
-    assert (a.size == N)
-    assert (np.all(a > 0))
-    
+    assert a.size == N
+    assert np.all(a > 0)
+
     if w == 0:
         return np.zeros([1, N], dtype=int)
     elif N == 1:
@@ -90,83 +115,114 @@ def anisoSmolyakMidSet(w, N, a):
     else:  # w>0 & N >1
         I = np.zeros((0, N), dtype=int)
         for comp1 in range(floor(w/a[0]) + 1):  # 0...floor(w/a[0])
-            rest = anisoSmolyakMidSet(w-comp1*a[0], N-1, a[1::])
-            newFirstCol = comp1*np.ones((rest.shape[0], 1), dtype=int)
-            newRows = np.concatenate((newFirstCol, rest), 1)
-            I = np.concatenate((I, newRows), 0)
+            rest = aniso_smolyak_mid_set(w-comp1*a[0], N-1, a[1::])
+            new_first_col = comp1*np.ones((rest.shape[0], 1), dtype=int)
+            new_rows = np.concatenate((new_first_col, rest), 1)
+            I = np.concatenate((I, new_rows), 0)
         return I
 
 
-def SmolyakMidSet(w, N):
-    """Generate the (isotropic) Smolyak multi-index set.
-        {{bnu in N_0^N : sum_i=1^N nu_i leq w}
+def smolyak_mid_set(w, N):
+    r"""Generate the (isotropic) Smolyak multi-index set, i.e.
 
+    .. math::
+        
+        \Lambda = \{\nu \in \mathbb{N}_0^N : \sum_{i=1}^N \nu_i \leq w\},
+
+    where we denoted :math:`\Lambda` the multi-index set, and :math:`w, N` the 
+    input ``w``, ``N``.
+    
     Args:
-        w (double or int): maximum l^infty norm of multi-index 
-        N (int): number of dimensions
-
+        w (float): Sizing parameter fo the multi-index set.
+        N (int): Number of dimensions.
+        
     Returns:
-        np.array: multi-index set (each row is a multi-index)
+        numpy.ndarray[int]: Multi-index set (each row is a multi-index of length
+        ``N``).
     """
 
     assert N >= 1
     a = np.repeat(1., N)
-    return anisoSmolyakMidSet(w, N, a)
+    return aniso_smolyak_mid_set(w, N, a)
 
-
-# TODO what if the user inputs a profit such that P_{e_n} rightarrow 0 as n rightarrow infty?
-# TODO rename it, to generic, must meniton OPTIMALITY WRT GAUSSIAN RFS
-def computeMidSetFast_freeDim(Profit, PMin):
-    """Compute midset based on a profit function and a minimum profit threshold:
-     Lambda = set{bnu in N^{infty} : PP_{bnu} geq PMin}.
-    NB Profit should be monotone (decreasing) wrt to the dimensions (otherwise
-    it is not possible to determine it) and the multi-indices (to obtain a
-        downward-closed multi-index set).
-    NB The present function also determines the maximum dimension possible, then 
-    calls the recursive part (next function) with fixed dimension.
+def mid_set_profit_free_dim(profit_fun, minimum_profit):
+    r"""Compute a midset based on a profit function ``profit_fun`` and a minimum 
+    profit threshold ``profit_fun`` as:
+    
+    .. math:: 
+    
+        \Lambda = \{\nu\in\mathbb{N}_0^{\infty}:
+        \mathcal{P}_{\nu}\geq P_{\textrm{min}}\},
+    
+    where we denoted
+    :math:`\Lambda` the multi-index set,
+    :math:`\mathcal{P}_{\nu}` the profit functiona pploed to a multi-index, and
+    :math:`P_{\textrm{min}}` the minimum profit threshold.
+    The profit should be monotone (decreasing) wrt to the dimensions (otherwise
+    it is not possible to determine it) and the multi-indices (to obtain a 
+    downward-closed multi-index set). 
+    This condition cannot be checked by the library, the user must ensure it.
+    The present function also determines the maximum dimension possible, then 
+    calls the :py:func:`compute_mid_set_fast` with fixed dimension.
 
     Args:
-        Profit (function): Get profit (positive double) for a multi-index. 
-            NB this function is required to vanish for increasing dimensions: 
-            P_{e_n} rightarrow 0 as n rightarrow infty
-            otherwise, the algorithm cannot terminate
-        PMin (double): minimum profit threshold for multi-index to be included
+        profit_fun (Callable[[numpy.ndarray[int]], float]): Get profit 
+            (positive) for a multi-index. 
+            This function is required to vanish for increasing dimensions: 
+            :math:`\mathcal{P}_{\mathbf{e}_n} \rightarrow 0` as 
+            :math:`n \rightarrow \infty`. Otherwise, the algorithm cannot 
+            terminate. This condition cannot be verified by the library and the
+            user must ensure it.
+        minimum_profit (float): minimum profit threshold for multi-index to be
+            included
 
     Returns:
-        2D array int: multi-index set (each row is a multi-index)
+        numpy.ndarray[int]: Multi-index set (each row is a multi-index).
     """
 
-    dMax = 1
-    nu = unitVector(dMax+1, dMax)
-    while (Profit(nu) > PMin):
-        dMax += 1
-        nu = unitVector(dMax+1, dMax)
+    d_max = 1
+    nu = unitVector(d_max+1, d_max)
+    while profit_fun(nu) > minimum_profit:
+        d_max += 1
+        nu = unitVector(d_max+1, d_max)
     # dMax is last that allows profit(e_{dMax}) > PMin
-    return computeMidSetFast(Profit, PMin, dMax)
+    return compute_mid_set_fast(profit_fun, minimum_profit, d_max)
 
-def computeMidSetFast(Profit, PMin, N):
-    """ Computes recrursively over N dimensions the multi-index set such that 
-    profit is above the threshold PMin:
-    Lambda = set{bnu in N^{infty} : PP_{bnu} geq PMin}.
-    NB Profit function should be monotone (decreasing) wrt to the multi-indices
+def compute_mid_set_fast(profit_fun, minimum_profit, N):
+    r""" Computes recrursively over ``N`` dimensions the multi-index set such 
+    that the profit ``profit_fun`` is above the threshold ``minimum_profit`` for
+    all its multi-indices. The multi-index set is defined as:
+
+    .. math:: 
+    
+        \Lambda 
+        = \{\nu\in\mathbb{N}_0^{N}:\mathcal{P}_{\nu}\geq P_{\textrm{min}}\}
+        
+    where we denoted
+    :math:`\Lambda` the multi-index set,
+    :math:`\mathcal{P}_{\nu}` the profit functiona pploed to a multi-index, and
+    :math:`P_{\textrm{min}}` the minimum profit threshold.
+    The profit function should be monotone (decreasing) wrt to the multi-indices
     to obtain a downward-closed multi-index set.
 
     Args:
-        Profit (function): Compute profit (positive double) for a multi-index
-        PMin (double): minimum profit threshold for multi-index to be included
+        profit_fun (Callable[[numpy.ndarray[int]], float]): Compute profit (positive
+            float) for a multi-index
+        minimum_profit (float): minimum profit threshold for multi-index to be included
         N (int): number of dimensions
 
     Returns:
-        2D array int: multi-index set (each row is a multi-index of length N)
+        numpy.ndarray[int]: Multi-index set (each row is a multi-index of length
+        ``N``).
     """
 
-    assert (N >= 0)
+    assert N >= 0
     if N == 0:
         return np.array([0], dtype=int)
     elif N == 1:  # this is the final case of the recursion
         nu = 0
-        assert (Profit(nu) > 0)
-        while (Profit(nu+1) > PMin):
+        assert (profit_fun(nu) > 0)
+        while (profit_fun(nu+1) > minimum_profit):
             nu = nu+1
         # at this point, nu is the largest mid with P > Pmin
         return np.linspace(0, nu, nu+1, dtype=int).reshape((-1, 1))
@@ -174,7 +230,7 @@ def computeMidSetFast(Profit, PMin, N):
         I = np.zeros((0, N), dtype=int)
         # check how large the LAST component can be
         nuNExtra = unitVector(N, N-1)  # has only a 1 in the last component
-        while (Profit(nuNExtra) > PMin):
+        while (profit_fun(nuNExtra) > minimum_profit):
             nuNExtra[-1] += 1
         maxLastComp = nuNExtra[-1] - 1
         # Recursively compute the other dimensions. Since I started with the 
@@ -183,8 +239,8 @@ def computeMidSetFast(Profit, PMin, N):
         for lastComponentNu in range(0, maxLastComp+1):
             nuCurr = np.zeros(N, dtype=int)
             nuCurr[-1] = lastComponentNu
-            PRec = PMin / Profit(nuCurr)
-            INewComps = computeMidSetFast(Profit, PRec, N-1)
+            PRec = minimum_profit / profit_fun(nuCurr)
+            INewComps = compute_mid_set_fast(profit_fun, PRec, N-1)
             INewMids = np.hstack(
                 (INewComps, np.full((INewComps.shape[0], 1), lastComponentNu)))
             I = np.vstack((I, INewMids))
--- sgmethods/multilevel_interpolant.py
+++ sgmethods/multilevel_interpolant.py
@@ -1,113 +1,130 @@
-
+"""Module for a class that implements the mmultilevel sparse grid interpolation
+from  `[Teckentrup, Jantsch, Webster, Gunzburger (2015)] 
+<https://epubs.siam.org/doi/abs/10.1137/140969002>`_.
+"""
 
 class MLInterpolant:
-    """Class for multi level sparse grid interpolant like in 
-    [Teckentrup, Jantsch, Webster, Gunzburger (2015)].
+    """Class for multilevel sparse grid interpolant.
     """
 
-    def __init__(self, SLIL):
-        """Store Single level interpolants list (that behave li SGInterpolant 
-        class)
+    def __init__(self, interpolants_sequence):
+        """Store the list of single-level interpolants (assumed to behave like
+        :Class:`SGInterpolant`).
 
         Args:
-            SLIL (list of SGInterpolant): List of sparse grid interpolants with
-            appropriate multi-index sets
-        """
-
-        self.nLevels = len(SLIL)
-        self.SLIL = SLIL
-
-    def sample(self, FApprox):
-        """Sample the MLInterpolant
-
-        Args:
-            FApprox (function): Function with 2 inputs: a parameter (1D double 
-            array of ANY length), and a level k (int >=0) that gives 
-            approximation accuracy level
+            interpolants_sequence (list): List of sparse grid interpolants with
+            appropriate multi-index sets.
 
         Returns:
-            list of 2D arrays double: List of length self.nLevels. Each list 
-            element is a 2D array; each row is a sample from FApprox for a 
-            parameter in the corresponding sparse grid; 
+            None
+        """
+
+        self.n_levels = len(interpolants_sequence)
+        self.interp_seq = interpolants_sequence
+
+    def sample(self, f_approx):
+        """Sample ``f_approx`` on the collocation nodes needed to compute the
+        multileve interpolant.
+
+        Args:
+            ``f_approx`` (Callable[[numpy.ndarray[float], int], float]):
+                Function with 2 inputs: A parameter (1D double array of any 
+                length), and a level ``k`` (int >=0) that gives approximation 
+                accuracy level.
+
+        Returns:
+            list: List of the same length as ``interpolants_sequence`` used to
+            construct the object. Each list element is a 2D numpy.ndarray. Each
+            row is a sample from ``f_approx`` for a parameter in the 
+            corresponding sparse grid.
         """
 
         # TODO: FApprox is only allowed to return values that are 1D arrays of
         #       FIXED length, indepednet of the level
         # TODO: Assert that FAppeox return onyly arrays of fixed length;
-        MLSamplesF = []
-        for k in range(self.nLevels):
-            def FApproxCurrLevel(y): return FApprox(y, k)
-            samplesCurrentLevel = self.SLIL[self.nLevels -
-                                            1-k].sampleOnSG(FApproxCurrLevel)
-            MLSamplesF.append(samplesCurrentLevel)
-        return MLSamplesF
+        ml_samples_f = []
+        for k in range(self.n_levels):
+            def f_approx_curr_level(y):
+                return f_approx(y, k)
+            samples_current_level = self.interp_seq[self.n_levels-1-k].\
+                sampleOnSG(f_approx_curr_level)
+            ml_samples_f.append(samples_current_level)
+        return ml_samples_f
 
-    def interpolate(self, yy, MLSamplesF):
-        """USe the interpolation oeprator to appeoximate the function F of which
-         ML samples are given
-
-        Args:
-            yy (2D array double): each row is a parameter and can have any 
-            length; number of rows also arbitrary
-            MLSamplesF (_type_): _description_
-
-        Returns:
-            2D array double: each row is the approximation of the function F in 
-            a parameter yy[i,:]
-        """
-
-        InterpolantOnYY = self.SLIL[0].interpolate(
-            yy, MLSamplesF[self.nLevels-1])  # first must be handeld out of loop
-        for k in range(1, self.nLevels):
-            samplesCurr = MLSamplesF[self.nLevels-1-k]
-            def FMock(y): return 1/0  # TODO write something more decent
-            samplesCurrReduced = self.SLIL[k-1].sampleOnSG(
-                FMock, oldXx=self.SLIL[k].SG, oldSamples=samplesCurr)
-            InterpolantOnYY += self.SLIL[k].interpolate(yy, samplesCurr) - \
-                self.SLIL[k-1].interpolate(yy, samplesCurrReduced)
-        return InterpolantOnYY
-
-    def getMLTerms(self, yy, MLSamplesF):
-        """Get terms of multi-level expansion split based on FE spaces
+    def interpolate(self, yy, ml_samples_f):
+        """Use the interpolation operator to approximate the function ``f`` of
+        which ML samples are given.
 
         Args:
-            yy (2D array double): each row is a parameter vector toe valuate
-            MLSamplesF (list of 2D arrays double): k-th entry is 2D array with 
-            shape nY x kth physical space size representing (I_{K-k}}-I_{K-k-1}[u_k])
+            yy (numpy.ndarray[float]): Each row is a parameter of any length. The
+                number of rows is also arbitrary.
+            ml_samples_f (list): List of multilevel samples as computed by the
+                method :py:meth:`sample`.
 
         Returns:
-            list of 2D arrays: k-th term is 2D array; each row corresponds to a
-            parameter in yy; each colum gives the finite element corrdinates
+            numpy.ndarray[double]: Each row is the approximation of ``f`` in the 
+            parameter given by the correspondg row of `yy`.
         """
 
-        MLTerms = []
-        for k in range(self.nLevels):
-            currSGLevel = self.nLevels-1-k
-            currVals = self.SLIL[currSGLevel].interpolate(yy, MLSamplesF[k])
+        interpolant_on_yy = self.interp_seq[0].interpolate(
+            yy, ml_samples_f[self.n_levels-1])  # first hande out of loop
+        for k in range(1, self.n_levels):
+            samples_curr = ml_samples_f[self.n_levels-1-k]
+            def f_mock(y):
+                return 1/0  # TODO write something more decent
+            samples_curr_reduced = self.interp_seq[k-1].sampleOnSG(
+                f_mock, oldXx=self.interp_seq[k].SG, oldSamples=samples_curr)
+            interpolant_on_yy += \
+                self.interp_seq[k].interpolate(yy, samples_curr) - \
+                self.interp_seq[k-1].interpolate(yy, samples_curr_reduced)
+        return interpolant_on_yy
+
+    def get_ml_terms(self, yy, ml_samples_f):
+        r"""Get terms of multi-level expansion split based on FE spaces.
+
+        Args:
+            yy (numpy.array[float]): each row is a parameter vector toe valuate
+                MLSamplesF (list of 2D arrays double). The k-th entry is a 2D 
+                array with shape `nY` x kth physical space size representing 
+                :math:`(I_{K-k}-I_{K-k-1})[u_k]`.
+
+        Returns:
+            [list]: k-th term is 2D array. Each row corresponds to a parameter 
+            in `yy`. Each colum gives the finite element corrdinates.
+        """
+
+        ml_terms = []
+        for k in range(self.n_levels):
+            curr_sg_level = self.n_levels-1-k
+            curr_vals = \
+                self.interp_seq[curr_sg_level].interpolate(yy, ml_samples_f[k])
             # NBB the last FE level coresponds to 1 interpolant since I_{-1}=0
-            if (k < self.nLevels-1):
-                def FMock(x): return 0.
-                MLSamplesReduceds = self.SLIL[currSGLevel-1].sampleOnSG(
-                    FMock, oldXx=self.SLIL[currSGLevel].SG,
-                    oldSamples=MLSamplesF[k])
-                currVals -= self.SLIL[currSGLevel -
-                                      1].interpolate(yy, MLSamplesReduceds)
-            MLTerms.append(currVals)
-        return MLTerms
+            if k < self.n_levels-1:
+                def f_mock(x):
+                    return 0.
+                ml_samples_reduceds = self.interp_seq[curr_sg_level-1].\
+                    sampleOnSG(f_mock, oldXx=self.interp_seq[curr_sg_level].SG,
+                               oldSamples=ml_samples_f[k])
+                curr_vals -= self.interp_seq[curr_sg_level -
+                                      1].interpolate(yy, ml_samples_reduceds)
+            ml_terms.append(curr_vals)
+        return ml_terms
 
-    def totalCost(self, costKK):
-        """Compute cost of computing this ML appeoximation
+    def total_cost(self, cost_kk):
+        """Compute cost of computing this ML appeoximation.
 
         Args:
-            costKK (1D array of double): kth entry is the cost of computing 1 FE
-            sample at level k
+            cost_kk (numpy.ndarray[float]): The k-th entry is the cost of 
+                computing 1 finite element sample at level k.
 
         Returns:
-            double: Total cost based on number of SG nodes, level, FE samples
+            float: Total cost based on number of sparse grid nodes, levels, and
+            finite element computations.
         """
 
-        assert (costKK.size == self.nLevels)
-        totalCostCurr = 0
-        for k in range(self.nLevels):
-            totalCostCurr += self.SLIL[k].numNodes * (costKK[self.nLevels-k-1])
-        return totalCostCurr
+        assert cost_kk.size == self.n_levels
+        total_cost_curr = 0
+        for k in range(self.n_levels):
+            total_cost_curr += \
+                self.interp_seq[k].numNodes * (cost_kk[self.n_levels-k-1])
+        return total_cost_curr
