ca50c5260b617ecdcc82f83ee16499bee7b61f70
Andrea Scaglioni <andrea.scaglioni@asc.tuwien.ac.at>
2024-10-11 17:18:47 +0200

    small changes towards examples

--- example/0_interpolate_scalar_function.py
+++ example/0_interpolate_scalar_function.py
@@ -1,39 +1,55 @@
-import numpy as np 
+""" We use SGMethods to interpolate a function with an infinite-dimensional 
+domain.
+"""
+
 from math import sin, sqrt
-from scipy.interpolate import RegularGridInterpolator
-import sys, os
-sys.path.insert(1, os.path.join(os.path.expanduser("~"), 'workspace/SGMethods'))
-from SGMethods.nodes_1d import unboundedKnotsNested
-from SGMethods.multi_index_sets import anisoSmolyakMidSet
-from SGMethods.sparse_grid_interpolant import SGInterpolant
+import sys
+# import os
+import numpy as np
+from sgmethods.nodes_1d import unbounded_nodes_nested
+from sgmethods.multi_index_sets import aniso_smolyak_mid_set
+from sgmethods.tp_inteprolants import TPPwLinearInterpolator
+from sgmethods.sparse_grid_interpolant import SGInterpolant
+
+sys.path.insert(1, 'sgmethods')
+
+# Weight function
+def weight_fun(N):
+    """Assign different importance to variables in  following function."""
+    np.power(np.linspace(1, N, N), -2)
 
 
-"""We use SGMethods to interpolate an infinite-dimensional (domain) parametetric map."""
-
-# High-dimensional function
-weightFun = lambda N : np.power(np.linspace(1, N, N), -2)  # Weight function
-def F(y):
-    weightCurr = weightFun(y.size)
-    return sin(np.dot(weightCurr, y))
+def f(y):
+    """High-dimensional function."""
+    return sin(np.dot(weight_fun(y.size), y))
 
 # Sparse grid parameters
-p = 2  # Polynomial degree + 1 
-TPInterpolant = (lambda nodesTuple, fOnNodes : 
-                 RegularGridInterpolator(nodesTuple, fOnNodes, method='linear', bounds_error=False, fill_value=None))
+p = 2  # Polynomial degree + 1
 lev2knots = lambda n: 2**(n+1)-1  # Level-to-knots function
-knots = lambda n : unboundedKnotsNested(n, p)  # Nodes family
 
 # Error computation
-def computeL2Error(uExa, Iu):
-    assert(uExa.shape[0] == Iu.shape[0])
-    return sqrt(np.mean(np.square(uExa - Iu)))
-yyRnd = np.random.normal(0, 1, [256, 1000])  # 256 random sample of (effectively) infinite parameter vector
-uExa = np.array(list(map(F, yyRnd)))  # Random sample of exact function
+def compute_L2_error(exact_u, interpol_u):
+    """ Compute the L2 error between the exact and the interpolated function."""
+    assert exact_u.shape[0] == interpol_u.shape[0]
+    return sqrt(np.mean(np.square(exact_u - interpol_u)))
+
+# Generate 256 random sample of (effectively) infinite parameter vector
+yy_rnd = np.random.normal(0, 1, [256, 1000])
+uExa = np.array(list(map(f, yy_rnd)))  # Random sample of exact function
 
 # Sparse grid interpolation
 anisoVec = lambda N : 2**(np.linspace(0, N-1, N))
-midSet = anisoSmolyakMidSet(w=5, N=10, a=anisoVec(10))  # Anisotropic Smolyak multi-index set in 10 dimensions
-interpolant = SGInterpolant(midSet, knots, lev2knots, TPInterpolant=TPInterpolant)
-uOnSG = interpolant.sampleOnSG(F)  # Sample the function on the sparse grid
-uInterp = interpolant.interpolate(yyRnd, uOnSG)  # Compute the interpolated value
-error = computeL2Error(uExa, uInterp)
+
+# Anisotropic Smolyak multi-index set in 10 dimensions
+mid_set = aniso_smolyak_mid_set(w=5, N=10, a=anisoVec(10))  
+
+# Define the sparse grid interpolant
+interpolant = SGInterpolant(mid_set, unbounded_nodes_nested, lev2knots,\
+                            tp_interpolant=TPPwLinearInterpolator)
+
+uOnSG = interpolant.sample_on_SG(f)  # Sample the function on the sparse grid
+
+u_interpol = interpolant.interpolate(yy_rnd, uOnSG)  # Interpolate on rnd sample
+
+error = compute_L2_error(uExa, u_interpol)
+print(error)
