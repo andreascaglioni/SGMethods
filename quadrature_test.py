5e2f46b3823ef0ec9a52ac7618e9356b1f3b9e17
Andrea Scaglioni <andrea.scaglioni@asc.tuwien.ac.at>
2025-07-02 08:35:24 +0200

    progress quadrture

--- quadrature_test.py
+++ quadrature_test.py
@@ -9,6 +9,11 @@ from sgmethods.quadrature import profit_sllg, compute_quadrature_params
 from sgmethods.multi_index_sets import tensor_product_mid_set
 from sgmethods.utils import float_f
 
+from sgmethods.quadrature import compute_1d_quadrature_weights
+from sgmethods.nodes_1d import opt_guass_nodes_nest
+from sgmethods.sparse_grid_interpolant import SGInterpolant
+from sgmethods.utils import rate
+
 
 def test_profit_definition():
     """
@@ -37,16 +42,16 @@ def test_profit_definition():
     plt.show()
 
 
-def test_quadrature_execution():
-    """Tests quadrature parameter computation and visualizes nodes and weights in 3D."""
+def test_quadrature():
+    """Tests computation quadrature nodes and weights. Plot in 3D."""
 
-    n, w = compute_quadrature_params(min_n_samples=10, dim_samples=2)
+    n, w = compute_quadrature_params(min_n_samples=6, dim=2)
 
-    print(f"{'Node':>11} | {'Weight':>8}")
+    print(f"{'Nodes':>11} | {'Weight':>8}")
     print("-" * 25)
     for node, weight in zip(n, w):
         node_str = " ".join(f"{x:5.2f}" for x in node)
-        print(f"{node_str:>10} | {weight:>8.4f}")
+        print(f"{node_str:>11} | {weight:>8.4f}")
 
     fig = plt.figure()
     ax = fig.add_subplot(111, projection="3d")
@@ -56,6 +61,45 @@ def test_quadrature_execution():
     ax.set_zlabel("w(y)")
     plt.title("3D Scatter Plot of nodes and weights")
     plt.show()
+
+
+def _conv_test_quadr(f, If, dim, min_nn):
+    nn = np.zeros_like(min_nn)
+    Qf = np.zeros_like(nn, dtype=float)
+    for i, n in enumerate(min_nn):
+        xx, ww = compute_quadrature_params(min_n_samples=n, dim=dim)
+        nn[i] = ww.size
+        Qf[i] = np.dot(f(xx.T), ww)
+    err = np.abs(If-Qf)
+    return nn, err
+
+def test_convergence_2d():
+    f = lambda x: x[0] ** 2  # noqa: E731
+    If = 1.
+    min_nn = 2**np.arange(1,11)
+    nn, err = _conv_test_quadr(f, If, 5, min_nn)
+    r = rate(err, nn)
+    print("n_xx", nn)
+    print("err", err)
+    print("Mean rate", np.mean(r))
+    plt.loglog(nn, err, '.-')
+    plt.loglog(nn, nn**(-np.mean(r)), 'k-')
+    plt.show()
+
+def test_conv_wrt_dim():
+    f = lambda x: x[0] ** 2  # noqa: E731
+    If = 1.
+    min_nn = 2**np.arange(1, 11, dtype=int)
+    dd = np.arange(1, 10, dtype=int)
+    rr = np.zeros_like(dd)
+    for i, dim in enumerate(dd):
+        nn, err = _conv_test_quadr(f, If, dim, min_nn)
+        r = rate(err, nn)
+        rr[i] = np.mean(r)
+    print(rr)
+
+
+
 
 
 def test_quadrature_simple_f():
@@ -73,7 +117,7 @@ def test_quadrature_simple_f():
     f = lambda x: 1 + 0 * x[0]  # noqa: E731
     If = 1
     print("f=1, If =", If)
-    n, w = compute_quadrature_params(min_n_samples=n_samples, dim_samples=d)
+    n, w = compute_quadrature_params(min_n_samples=n_samples, dim=d)
     assert n.shape[0] >= n_samples, "not enough samples"
     If_approx = np.dot(w, f(n.T))
     print("If=", If, "; If approx =", If_approx, "; Error =", np.abs(If - If_approx))
@@ -89,7 +133,7 @@ def test_quadrature_simple_f():
     f = lambda x: x**2  # noqa: E731
     If = 1
     print("f=x**2, If =", If)
-    n, w = compute_quadrature_params(min_n_samples=n_samples, dim_samples=d)
+    n, w = compute_quadrature_params(min_n_samples=n_samples, dim=d)
     assert n.shape[0] >= n_samples, "not enough samples"
     If_approx = np.dot(w, np.squeeze(f(n.T)))
     print(
@@ -110,7 +154,7 @@ def test_conv_quadr_simple_f():
     for eps in [1.0e-1, 1.0e-2, 1.0e-3]:
         print("eps:", eps)
         for i, ns in enumerate(nns):
-            n, w = compute_quadrature_params(min_n_samples=ns, dim_samples=N, eps=eps)
+            n, w = compute_quadrature_params(min_n_samples=ns, dim=N, eps=eps)
             If_approx = np.dot(w, f(n.T).squeeze())
             err[i] = np.abs(If - If_approx)
             print(
@@ -144,7 +188,7 @@ def test_convergence_mean_error():
     for i, ns in enumerate(nns):
         errs = []
         for _ in range(n_trials):
-            n, w = compute_quadrature_params(min_n_samples=ns, dim_samples=N, eps=eps)
+            n, w = compute_quadrature_params(min_n_samples=ns, dim=N, eps=eps)
             If_approx = np.dot(w, f(n.T).squeeze())
             errs.append(np.abs(If - If_approx))
         mean_err[i] = np.mean(errs)
@@ -163,11 +207,36 @@ def test_convergence_mean_error():
     plt.show()
 
 
-if __name__ == "main":
-    # test_profit_definition()
-    test_quadrature_execution()
-    # test_quadrature_constant_f()
-    # test_quadrature_identity()
-    # test_quadrature_square()
-    # test_convergence()
-    # test_convergence_mean_error()
+def test_1d_weights():
+    f = lambda x: x**2  # f = x**2 -> int f d mu = 1
+    # f = lambda x : x  # f = x -> int f d mu = 0
+    # f = lambda x : np.ones_like(x)  # f = 1 -> int f d mu = 1
+    lev2knots = lambda nu: 2 ** (nu + 1) - 1  # noqa: E731
+    knots = lambda m: opt_guass_nodes_nest(m)  # noqa: E731
+    max_nu = 10
+    ww = compute_1d_quadrature_weights(max_nu, lev2knots, knots)
+    Q_f = np.zeros(max_nu)
+    for nu in range(max_nu):
+        ww_curr = ww[nu]
+        print(ww_curr)
+        mid_set = np.arange(nu + 1, dtype=int).reshape((-1, 1))
+        I = SGInterpolant(mid_set, knots, lev2knots)
+        sg = I.SG.flatten()
+        f_sg = f(sg)
+        Q_f[nu] = np.dot(f_sg, ww_curr)
+    print(Q_f)
+    ll = np.array([ww[nu].size for nu in range(max_nu)])
+    plt.loglog(ll, Q_f - 1.0, ".-")
+    plt.loglog(ll, 1.0 / ll**2, "k-")
+    plt.show()
+
+
+# if __name__ == "main":
+# test_profit_definition()
+# test_quadrature()
+# test_quadrature_simple_f()
+# test_convergence()
+# test_convergence_mean_error()
+# test_1d_weights()
+# test_convergence_2d()
+test_conv_wrt_dim()
--- sgmethods/quadrature.py
+++ sgmethods/quadrature.py
@@ -5,20 +5,19 @@ from sgmethods.nodes_1d import opt_guass_nodes_nest
 from sgmethods.multi_index_sets import compute_mid_set_fast
 import numbers
 import warnings
-from scipy.stats import norm
+from scipy.special import erf
 
 
 def profit_sllg(nu, p=2):
-    """Compute multi-index profits for sparse grid interpolant of parametric
-    SLLG.
+    """Compute the sparse grid profits of given multi-indices.
+    For piecewise-polynomials of degree p-1 and parametric SLLG.
 
     Args:
         nu (numpy.ndarray[float]): 2D array, each row is a multi-index.
         p (int): Integer >= 2. Piecewise polynomial interpolation degree + 1.
 
     Returns:
-        numpy.ndarray[float]: 1D array of (non-negative) profits. Length equals
-        number of rows of nu.
+        numpy.ndarray[float]: 1D array of (non-negative) profits. Length equals number of rows of nu.
     """
 
     #  Check input
@@ -36,29 +35,29 @@ def profit_sllg(nu, p=2):
     N_nu, D = nu.shape
     C1, C2 = 1, 1
 
-    w1 = np.asarray(nu == 1).nonzero()  # np.where(nu == 1)
-    w2 = np.asarray(nu > 1).nonzero()  # np.where(nu > 1)
+    w1 = np.asarray(nu == 1).nonzero()
+    w2 = np.asarray(nu > 1).nonzero()
 
-    # Levels Levy-Ciesielski expansion (same shape as nuwith np.repeat)
-    nn = np.arange(1, D + 1, 1)  # linear indices mid in nu from (1)
-    ell = np.ceil(np.log2(nn))  # log-indices mid in nu (from 0)
+    # Levels Levy-Ciesielski expansion (same shape as nu)
+    nn = np.arange(1, D + 1, 1)  # linear indices mids
+    ell = np.ceil(np.log2(nn))  # log-indices mids
     ell = np.reshape(ell, (1, ell.size))
     ell = np.repeat(ell, N_nu, axis=0)
 
-    # The regularity weights (radious of domain of holomorphic extension)
+    # The regularity weights (radii of C-disks of holomorphic extension)
     rho = np.zeros_like(nu, dtype=float)
-    if not (w1[0].size == 0):  # if 1 tuple element is empty, the other is too
+    if not (w1[0].size == 0):  # if entry 0 is empty, the other is too
         rho[w1] = 2.0 ** (3.0 / 2.0 * ell[w1])
     if not (w2[0].size == 0):
         rho[w2] = 2.0 ** (1.0 / 2.0 * ell[w2])
 
-    # Compute value of components of each multi-index (multiply -> value multi-index)
+    # Compute value
     V_comps = np.ones_like(rho)
     V_comps[w1] = C1 * np.power(rho[w1], -1.0)
     V_comps[w2] = C2 * np.power(2.0, -p * nu[w2] * rho[w2])
     value = np.prod(V_comps, axis=1)
 
-    # Work of a multi-index. Computed with product structure, as for work.
+    # Compute work
     W_comps = (2 ** (nu + 1) - 2) * (p - 1) + 1
     work = np.prod(W_comps, axis=1)
 
@@ -67,24 +66,17 @@ def profit_sllg(nu, p=2):
 
 # TODO Currently assuming Gaussian samples for SLLG. Add more options/make an input
 # TODO improve efficiency computation integral: instead of MC, do exact computation based on inclusion-exclsion and TP structure
-def compute_quadrature_params(
-    min_n_samples, dim_samples, distrbution="gauss", eps=1.0e-2
-):
+def compute_quadrature_params(min_n_samples, dim, distrbution="gauss", eps=1.0e-2):
     """
     Computes quadrature nodes and weights for sparse grid quadrature using a Gaussian distribution.
     Args:
-        min_n_samples (int): Minimum number of quadrature nodes requested.
-        dim_samples (int): Number of dimensions for the quadrature.
+        min_n_samples (int): Minimum number of desired quadrature nodes.
+        dim (int): Number of dimensions for the quadrature. 
         distrbution (str, optional): Integral measure. Only "gauss" is supported for now. Defaults to "gauss".
         eps (float, optional): Accuracy integration basis functions. Defaults to 1.0e-4.
     Returns:
-        tuple:
-            quad_nodes (np.ndarray): Array of quadrature nodes (sparse grid points).
-            quad_weights (np.ndarray): Array of quadrature weights corresponding to the nodes.
-    Raises:
-        ValueError: If `min_n_samples` is not a positive number.
-        ValueError: If `dim_samples` is not a positive integer.
-        ValueError: If `distrbution` is not "gauss".
+        quad_nodes (np.ndarray): Array of quadrature nodes (sparse grid points of dimension `dim`).
+        quad_weights (np.ndarray): Array of quadrature weights corresponding to the nodes.
     Notes:
         - The quadrature weights are with inclusion-exclusion formula
         - The function relies on external functions/classes: `opt_guass_nodes_nest`, `profit_sllg`, `compute_mid_set_fast`, and `SGInterpolant`.
@@ -93,7 +85,7 @@ def compute_quadrature_params(
     # Check input
     if not isinstance(min_n_samples, numbers.Number) or min_n_samples <= 0:
         raise ValueError("n_samples must be a positive integer.")
-    if not isinstance(dim_samples, int) or dim_samples <= 0:
+    if not (isinstance(dim, numbers.Number) ) or dim <= 0:
         raise ValueError("dim_samples must be a positive integer.")
     if distrbution != "gauss":
         raise ValueError("Only 'gauss' distribution is supported.")
@@ -103,25 +95,30 @@ def compute_quadrature_params(
     P = profit_sllg
 
     # Find mid_set such that: #SG > min_n_samples
-    min_p = P(np.zeros((1, 1), dtype=int))[0]  # include all mids : profit > min_p
-    decrease_min_p = True
-    while decrease_min_p:
-        mid_set = compute_mid_set_fast(P, min_p, dim_samples)
-        I = SGInterpolant(mid_set, knots, lev2knots)
-        if I.num_nodes > min_n_samples:
-            decrease_min_p = False
-        else:
+    if min_n_samples == 1:
+        mid_set = np.array([[0]])
+        I =SGInterpolant(mid_set, knots, lev2knots)
+    else:
+        mid0 = np.zeros((1, 1), dtype=int)
+        min_p = P(mid0)[0]
+        while True:
+            # NB Settgin N=dim guarantees dimension does not grow beyoind dim
+            mid_set = compute_mid_set_fast(P, min_p, dim)
+            I = SGInterpolant(mid_set, knots, lev2knots)
+            if I.num_nodes > min_n_samples:
+                break
             min_p *= 0.5
 
     # Quadarature samples = current sparse grid
-    quad_nodes = I.SG
+    quad_nodes = np.zeros((I.num_nodes, dim))
+    quad_nodes += I.SG
 
-    quad_weights = compute_quadarture_wights_MC(dim_samples, eps, I)
+    quad_weights = compute_quadrature_wights_incl_excl(dim, eps, I)
 
     return quad_nodes, quad_weights
 
 
-def compute_quadrature_wights_incl_excl(dim_samples, eps, sg_interp):
+def compute_quadrature_wights_incl_excl(dim, eps, sg_interp):
     """Compute quadrature weights for sparse grid quadrature using the
     inclusion-exclusion formula.
 
@@ -135,92 +132,76 @@ def compute_quadrature_wights_incl_excl(dim_samples, eps, sg_interp):
     """
 
     sg = sg_interp.SG
-    num_nodes = sg.interp.num_nodes
-    combi_coeffs = sg.combination_coeffs
-    num_mids, dim = sg_interp.mid_set.shape
+    num_nodes = sg_interp.num_nodes
+    max_nu = np.amax(sg_interp.mid_set.flatten())
     lev2knots = sg_interp.lev2knots
-
+    knots = sg_interp.knots
     ww = np.zeros(num_nodes)  # quadrature weights, to return
-    
-    # Change shape combi_coeffs to (1, -1) for breoadcasting later
-    combi_coeffs = combi_coeffs.reshape(1, -1)
-    
-    # ------------------------------------------------------------------------ #
-    #                       Compute 1d quadrature weights                      #
-    # ------------------------------------------------------------------------ #
-    max_n = np.amax(sg_interp.mid_set.flatten())
-    W_1d = compute_1d_quadrature_weights(max_n, sg_interp)
-    
-    # ------------------------------------------------------------------------ #
-    #                       Compute TP quadrature weights                      #
-    # ------------------------------------------------------------------------ #
-    # For each *actuve* multi-index in the multi-index set
-    # Compute a matrix W_tp_nu of shape (lev2knots(nu[0]), ..., lev2knots(nu[-1]))
-    # W_tp_nu[i[0], ..., i[-1]] = \int_{\Gamma} l^{\bnu}_{\by} d\mu
-    # where \by = [kk[0, i[0]], ..., k[-1, i[-1]]]
-    # and k[j, :] are the 1d nodes corresponding to nu[j]
-    # TODO how use 1d quadrature weights?
-    
-    W_tp = np.zeros(num_mids)
-    for i in range(W_tp):
-        W_1d = np.zeros(lev2knots())
 
-    # TODO convert to SG shape:
-    #     (W_tp)_ij = \int_{\Gamma} l_{\by_i}^{\bnu_j} d\mu
-    #     W_tp = np.zeros(num_nodes, num_mids)
-    W_tp = None
-    
-    #  Cmpute SG weights with inclusion-exclusion formula
-    ww = np.sum(combi_coeffs * W_tp, axis=1)  # bradcast combi_coeffs
+    # Compute 1d quadrature weights
+    W_1d = compute_1d_quadrature_weights(max_nu, lev2knots, knots)
 
+    # Inclusion-exclusion formula: Loop over active mids
+    for i, nu in enumerate(sg_interp.active_mids):
+        # 1 Compute TP quadrature weight from I_{nu}
+        dim_nu = nu.shape[0]
+        w_1d = [W_1d[nu[d]] for d in range(dim_nu)]
+        w_tp = np.array(np.meshgrid(*w_1d, indexing="ij"))
+        w_tp = np.prod(w_tp, axis=0)
+        w_tp_flat = w_tp.reshape(-1)
+
+        # TODO sg_interp.where_nu[i]: where the TP nodes of level bnu_i (ACTIVE mid) are located in tp.interp.SG
+        # Add to SG weights with inclusion-exclusion formula
+        ww[sg_interp.where_tp[i]] += sg_interp.combination_coeffs[i] * w_tp_flat
     return ww
 
-def compute_1d_quadrature_weights(max_n, lev2knots, knots, interpolant="pw_lin_extrapolated"):
-    """ Compute 1D quadrature weights associated to an interpolation rule, up to a max number of quadrature nodes.
 
-    Args:
-        max_n (int): The maximum index (exclusive) for which to compute quadrature weights.
-        lev2knots (callable[int, [int]]): Maps an index to the number of knots.
-        knots (callable[int, [np.ndarray[float]]]): The 1D knots, given the desired number from `lev2knots`.
-        interpolant (str, optional): The 1D interpolation. Defaults to / curretnly only supports "pw_lin_extrapolated" (piecewise linear interpolant with extrapolation).
+def compute_1d_quadrature_weights(
+    max_nu, lev2knots, knots, interpolant="pw_lin_extr"
+):
+    if interpolant != "pw_lin_extr":
+        raise NotImplementedError("For now only pw. lin.+extrapolation supported.")
 
-    Returns:
-        np.ndarray[np.ndarray[float]]: 1D array of objects. Each entry corresponds to an index up to `max_n`. The j-th entry is an array of 1D quadrature weights corresponding to the 1D nodes for the j-th index.
-    """
-    
-    assert interpolant == "pw_lin_extrapolated", "Currently only pw_lin_extrapolated interpolant supported."
+    def prim_integrand(a, b, x):
+        # With SimPy: Primitive of (ax+b)* e^(-x^2/2)
+        p = -a * np.exp(-(x**2) / 2) + sqrt(2 * pi) * b * erf(sqrt(2) * x / 2) / 2
+        p /= sqrt(2 * pi)
+        return p
 
-    prim_integrand = lambda a, b, x : -a/sqrt(2*pi)*exp**(-x**2/2) + sqrt(pi/2)*b*erf(x/sqrt(2))
-
-    ww_1d = np.empty(max_n, dtype=object)
-    for n in range(max_n):  # index
-        num_nodes = lev2knots(n)
-        nodes = knots(num_nodes)
-        ww_1d[n] = np.zeros(num_nodes)
-        if num_nodes == 0:
-            ww_1d[0] = np.array([1.0])
+    ww_1d = []
+    for nu in range(max_nu+1):  # include max_nu
+        n_nodes = lev2knots(nu)
+        ww_curr = np.zeros(n_nodes)
+        nodes = knots(n_nodes)
+        if n_nodes == 1:  # constant itnerpolation
+            ww_curr = np.array([1.0])
         else:
-            # Node 0
-            y, yn = nodes[0], nodes[1]
-            f = lambda x : prim_integrand(1/(y-yn), -yn/(y-yn), x)
-            int_y_yn = f[yn]-f[y]
-            ww_1d[n][0] = norm.cdf(nodes[0]) + int_y_yn
-
-
-
-
-
-
-
-            raise NotImplementedError("Only 'pw_lin_extrapolated' interpolant is supported.")
+            # Integrate to the LEFT of current node
+            nodes_left = np.concatenate(([-np.inf], nodes[0:-1]))
+            a = 1 / (nodes - nodes_left)
+            a[0] = -1 / (nodes[1] - nodes[0])  # edit for leftmost integral (unbounded)
+            b = -nodes_left * a
+            b[0] = -nodes[1] / (nodes[1] - nodes[0])  # edit for leftmost integral
+            nodes_left[1] = -np.inf  # the second (idx 1) basis function also unbounded
+            int_left = prim_integrand(a, b, nodes) - prim_integrand(a, b, nodes_left)
+            ww_curr += int_left
+            # Integrate to the RIGHT of current node ASSUMING simmetry basis functions
+            int_right = int_left[::-1]
+            ww_curr += int_right
+        ww_1d.append(ww_curr)
     return ww_1d
 
 
 
-# DEPRECATED (inefficient)
+
+
+
+
+
+# ------------------------- DEPRECATED (inefficient) ------------------------ #
 def compute_quadarture_wights_MC(dim_samples, eps, sg_interp):
     """Compute quadrature weights of sparse grid quadrature as :
-        weights[i] = \int_{\Gamma} L_{y_i} d\mu,
+        weights[i] = int_{Gamma} L_{y_i} d mu,
     where L_{y} denotes a Lagrange basis function of I.
     Observe that:
         L_{y_i} = I[delta_{y_i}],
